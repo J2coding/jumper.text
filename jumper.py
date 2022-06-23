537c9b7bdc02b875405c0dee6eeae13fde0ba761
J2coding <kimmyjmueni@gmail.com>
2022-06-23 22:58:04 +0300

    design

---
+++ jumper.py
@@ -0,0 +1,32 @@
+'''
+Classes
+- class Jumper
+-   The game itself
+- class ParachuteMan
+-   Drawing the parachute man
+- class GameOver
+-   For when the game is over
+- class Word
+-   Guessing the random word
+
+class:Jumper
+playing: bool (private)
+
+constructor()
+start()
+
+class: Word
+get_input()
+  get_random_word()
+  check_input()
+
+class ParachuteMan:
+  _lives: integer (private)
+
+  draw_parachute_man()
+  get_lives()
+
+class GameOver:
+  game_over()
+
+  '''
